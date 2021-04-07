0b95113daa55649e7ba9eb0e9daebdffb05dae56
Ragip Mullamusa <69114250+fonixweb@users.noreply.github.com>
2021-04-08 00:23:25 +0300

    Add files via upload

---
+++ Info.java
@@ -0,0 +1,32 @@
+package enosapp.fonixweb.com;
+
+
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+/*
+
+FonixWeb & RAGIP MULLAMUSA 
+My Notes - Notepad is an easy-to-use, intuitive, fast, elegant and secure notebook with cloud sync. 
+You can use My Notes as a notebook, diary, agenda, or diary. With the addition of images and links 
+Add a title 
+Add a subtitle 
+Add text 
+Add image 
+Add a link 
+Note modification 
+Delete note 
+Search 
+Link : https://play.google.com/store/apps/details?id=notes.fonixweb.com 
+ 
+facebook : facebook.com/ragip.mullamusa 
+Instagram : instagram.com/fonixweb 
+Youtube : youtube.com/fonixweb 
+Linkedin : linkedin.com/in/fonixweb 
+Twitter : twitter.com/fonix_web 
+ 
+Google : fonixweb.com
+ 
+*/
+    }
+}
